38ea1b94a10e13986060ad8ec73af977a7c74b66
Uliana-C <tyk240405@gmail.com>
2024-12-18 22:56:20 +0300

    добавлена функция сложения

--- main1.py
+++ main1.py
@@ -3,9 +3,32 @@ class Polynomial:
     def init(self, cof):
         # Делаем список
         self.cof = list(cof)
-        # Убираем ненужные нули в конце
+        # Убираем ненужные нули в конце и список делаем
         self._remove_leading_zeros()
 
     def _remove_leading_zeros(self):
-        while len(self.cof) > 1 and self.cof[-1] == 0:
-         
+        self.coeff = list(coeff)
+        self._remove_leading_zeros()
+
+    # Убираем нули в конце списка коэффициентов
+    def _remove_leading_zeros(self):
+        while len(self.coeff) > 1 and self.coeff[-1] == 0:
+            self.coeff.pop()
+
+    # Сложение 2 многочленов
+    def __add__(self, second_poly):
+        first_poly = self
+
+        #Длина списка
+        len_first = len(first_poly.coeff)
+        len_second = len(second_poly.coeff)
+        max_len = max(len_first, len_second)
+        # Дополняем нулями если нужн
+        padded_first = first_poly.coeff + [0] * (max_len - len_first)
+        padded_second = second_poly.coeff + [0] * (max_len - len_second)
+        # Складываем коэффициенты
+        result_coeff = [padded_first[i] + padded_second[i] for i in range(max_len)]
+        return Polynomial(result_coeff)
+
+
+
