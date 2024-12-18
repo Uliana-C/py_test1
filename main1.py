439a69c1bfd51196b5305def416b82cab54d2963
Uliana-C <tyk240405@gmail.com>
2024-12-18 23:02:46 +0300

    добавлена производная и видоизменен многочлен

--- main1.py
+++ main1.py
@@ -30,5 +30,47 @@ class Polynomial:
         result_coeff = [padded_first[i] + padded_second[i] for i in range(max_len)]
         return Polynomial(result_coeff)
 
+    # Вычисление значения многочлена в точке x
+    def evaluate(self, x):
+        result = 0  # Начинаем с 0
+        # Идем по коэффициентам и степеням
+        for power, coeff in enumerate(self.coeff):
+            result += coeff * x ** power  # Вычисляем значение
+        return result
+
+    #Представим многочлен в виде строки
+    def __str__(self):
+        if not self.coeff:
+            return "0"
+
+        terms = []  # пустой список
+        # Перебираем коэффициенты и степени
+        for power, coeff in enumerate(self.coeff):
+            if coeff == 0:
+                continue  # Пропускаем нулевые коэффициенты
+
+            if power == 0:
+                term = f"{coeff}"  # свободный член
+            elif power == 1:
+                term = f"{coeff}x"  # член с x
+            else:
+                term = f"{coeff}x^{power}"  # член со степенью больше 1
+            terms.append(term)  # добавляем строку в список
+
+        # Соединяем строки в одну
+        return " + ".join(reversed(terms)).replace(" + -", " - ").replace("+ -", "- ")
+
+    #Найдем производную
+    def derivative(self):
+        # если многочлен константа - производная  0
+        if len(self.coeff) <= 1:
+            return Polynomial([0])
+
+        derivative_coeffs = []
+        for power, coeff in enumerate(self.coeff):
+            if power > 0:
+                derivative_coeffs.append(coeff * power)
+
+        return Polynomial(derivative_coeffs)
 
 
